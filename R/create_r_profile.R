aaf7b1b9c33f2db29e18ab404725c2f73ceda378
mcdelaney <mcdelaney@gmail.com>
2016-02-20 20:03:23 +0000

    delete existing Rprofile if found

--- R/create_r_profile.R
+++ R/create_r_profile.R
@@ -5,6 +5,7 @@ create_r_profile <- function(mattlib_loc){
   file_loc <- normalizePath(getwd())
   mattlib_loc <- normalizePath(mattlib_loc)
   file <- sprintf("%s/.Rprofile", file_loc)
+  if (file.exists(file)) { system(sprintf("rm %s", file)) }
   file.create(file)
   writeLines(sprintf(".libPaths('%s/')", file_loc, mattlib_loc))
 }
